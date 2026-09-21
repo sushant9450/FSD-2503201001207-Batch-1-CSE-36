e22bb70cf6e65755135381672424fe41d3ff1fd8
Sushant Singh <sushantgkp9450@gmail.com>
2026-09-21 10:57:47 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
-
-import './App.css'
-
-function Title(){
-  return <h1> I am the best Coder of the World</h1>;
-}
-
-function Description(){
-  return <h2>This is the Description box</h2>
-}
-function App() {
+function App(){
   return (
-    <div> This is my component
-          <Title/>     {/* use to render  */}
-          <Title/>     {/* dubara same h1 wali line ko print krne ke leye  */}
-          <Description/>
-         </div>
+    <h1>Hello World from React using Vite</h1>
   );
 }
-
-export default App
+export default App;
